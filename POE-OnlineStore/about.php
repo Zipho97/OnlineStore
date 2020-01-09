99401e9a81864cedfe5ce030af72fb176f01c0dc
Zipho97 <50981794+Zipho97@users.noreply.github.com>
2020-01-09 22:46:38 +0200

    Add files via upload

---
+++ POE-OnlineStore/about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="test_site/css/style.css">
+  <link rel="stylesheet" href="test_site/css/menu.css">
+  <title>My online store</title>
+</head>
+
+<body>
+  <div class="menu-wrap">
+    <input type="checkbox" class="toggler">
+    <div class="hamburger">
+      <div></div>
+    </div>
+    <div class="menu">
+      <div>
+        <div>
+          <ul>
+            <li><a href="index.php">Shop</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <header class="showcase">
+    <div class="container showcase-inner">
+      <h1>About My Online Store</h1>
+      
+      <p> MOST EPIC online shopping mall, founded in 2019. Our goal is to be South Africa's leading shopping site.
+It is our mission to be customer focused to create an epic and easy place to buy anything online. </p>
+      <a href="index.php" class="btn">Go to store</a>
+    </div>
+  </header>
+</body>
+
+</html>
